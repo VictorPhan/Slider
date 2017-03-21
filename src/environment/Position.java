2b1419248c036cf0f2f1aea3535796e9a25bb877
tinbao <tinb@student.unimelb.edu.au>
2017-03-21 14:54:33 +1100

    added the bigInt array

--- src/environment/Position.java
+++ src/environment/Position.java
@@ -1,5 +1,7 @@
 package environment;
 
+import java.math.BigInteger;
+
 /**
  * The current position of the game space
  * @author TB VP
@@ -9,7 +11,12 @@ public class Position implements Consts {
 	
 	public static int dimen;
 	private long[] pieces = new long[piecesDimensionality];
+	private BigInteger[] bigPieces = new BigInteger[piecesDimensionality];
 	MoveList ml;
+	public static final int V = 0;
+	public static final int H = 1;
+	public static final int B = 2;
+	public static int dimension;
 	
 	/**
 	 * Constructor initializing dimension and initial position
@@ -24,19 +31,37 @@ public class Position implements Consts {
 	
 	/**
 	 * Constructor for position
-	 * @param pieces
+	 * @param pieces 
 	 */
 	public Position(long [] pieces) {
 		this.pieces = pieces;
 		ml = new MoveList(pieces);
 	}
 	
-	public long [] getPieces() {
-		return pieces;
+	/**
+	 * Constructor for n > 8 case
+	 * @param dimension 
+	 * @param bigPieces
+	 */
+	public Position(int dimension, BigInteger[] bigPieces){
+		Position.dimension = dimension;
+		this.bigPieces = bigPieces;
 	}
 	
+	/**
+	 * @param i index postion of the piece
+	 * @return A specific piece by i
+	 */
 	public long getPieces(int i) {
 		return pieces[i];
+	}
+	
+	public BigInteger getBigPieces(int i) {
+		return bigPieces[i];
+	}
+	
+	public long [] getPieces() {
+		return pieces;
 	}
 
 	public void setPieces(long[] pieces) {
@@ -45,5 +70,13 @@ public class Position implements Consts {
 
 	public static int getdimen() {
 		return dimen;
+	}
+
+	public BigInteger[] getBigPieces() {
+		return bigPieces;
+	}
+
+	public void setBigPieces(BigInteger[] bigPieces) {
+		this.bigPieces = bigPieces;
 	}
 }
