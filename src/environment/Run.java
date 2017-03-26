2ea1e9c0be8100d6eb82229f1f41962a073909fa
tinbao <tinb@student.unimelb.edu.au>
2017-03-26 22:59:21 +1100

    [1.0.1] Added Runtime print

--- src/environment/Run.java
+++ src/environment/Run.java
@@ -8,10 +8,14 @@ package environment;
 public class Run implements Consts {
 	
 	public static void main(String[] args) {
+		long startTime = System.nanoTime();
 		
 		Position init = Parse.parseBoard();
 		System.out.println(init.ml.nHMoves());
 		System.out.println(init.ml.nVMoves());
 		
+		long endTime   = System.nanoTime();
+		long totalTime = (endTime - startTime) / 1000000;
+		System.out.println("Runtime = " + totalTime + "ms");
 	}
 }
