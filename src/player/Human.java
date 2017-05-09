050208e83dd0b4f89e2916d5a951541c72012906
tinbao <tinb@student.unimelb.edu.au>
2017-05-09 23:43:47 +1000

    forgot the errors in human.java

--- src/player/Human.java
+++ src/player/Human.java
@@ -6,6 +6,7 @@ import environment.Parse;
 import environment.Position;
 import environment.Side;
 import exceptions.InvalidMoveException;
+import top_end.Move;
 
 public class Human extends Player {
 	
@@ -93,6 +94,24 @@ public class Human extends Player {
 		} catch (InvalidMoveException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void init(int dimension, String board, char player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Move move) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Move move() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
